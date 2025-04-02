aaf4386d014402e83cd7c3373bcd6e5eadfa5ff0
Madusha-navod <124715943+Madusha-navod@users.noreply.github.com>
2025-04-02 18:11:08 +0000

    ChatApp route create

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import ChatApp from "./components/ChatApp";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={< Welcome />}/>
       <Route path="/signup" element={< Signup />} />
-      <Route path="/login" element={< Login/>} />
+      <Route path="/login" element={< Login />} />
+      <Route path="/chatapp" element={< ChatApp />} />
     </Routes>
    </Router>
   );
